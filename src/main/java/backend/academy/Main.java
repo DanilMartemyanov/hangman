75327aa11b8b445720fbc60602e771e51b02820d
DanilMartemyanov <martidanilka@gmail.com>
2024-09-05 17:06:33 +0300

    add method getRandomLevel and getRandomCategory

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 
+import gallowsGame.WordsGallows;
 import lombok.experimental.UtilityClass;
 import java.io.File;
 import java.io.IOException;
@@ -16,13 +17,18 @@ public class Main {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             JsonNode jsonNode = objectMapper.readTree(file);
-            for(JsonNode i : jsonNode.get("category")){
-                System.out.println(i.get("title"));
+            for(JsonNode i : jsonNode.get("category").findValues("level")){
+                System.out.println(i);
             }
         } catch (IOException e) {
             //TO DO: реализовать свой класс Exctption
             throw new RuntimeException(e);
         }
+
+        WordsGallows wordsGallows = new WordsGallows();
+        System.out.println(wordsGallows.getRandomCategory());
+        System.out.println(wordsGallows.getRandomLevel());
+
 
     }
 }
--- src/main/java/gallowsGame/WordsGallows.java
+++ src/main/java/gallowsGame/WordsGallows.java
@@ -9,7 +9,11 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class WordsGallows {
-    private static File fileWords = new File("src/recources/words.json");
+    private static final File fileWords = new File("src/main/resources/words.json");
+//    private static final char EASY = 0;
+//    private static final char MIDDLE = 1;
+//    private static final char HARD = 3;
+
     public String getWords(ArrayList<String> words) {
         int quantity = words.size();
         int index = ThreadLocalRandom.current().nextInt(0, quantity + 1);
@@ -17,19 +21,43 @@ public class WordsGallows {
 
     }
 
-    public String getRandomWordsforGame(){
+    public String getRandomCategory() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            JsonNode jsonNode = objectMapper.readTree(fileWords);
+            JsonNode node = objectMapper.readTree(fileWords);
+            ArrayList<String> category = new ArrayList<>();
+            for (JsonNode jsonNode1 : node.get("category")) {
+                category.add(jsonNode1.get("title").asText());
+            }
+            int index = ThreadLocalRandom.current().nextInt(0, category.size());
+            return category.get(index);
         } catch (IOException e) {
             //TO DO: реализовать свой класс Exctption
             throw new RuntimeException(e);
         }
 
-        return null;
     }
 
-    public String getRandomCategory(){
-        return null;
+    public String getRandomLevel() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode node = objectMapper.readTree(fileWords);
+            ArrayList<String> level = new ArrayList<>();
+            int sizeLevelArray = node.get("category").findValues("level").size();
+            int index = ThreadLocalRandom.current().nextInt(0, sizeLevelArray);
+            switch (index) {
+                case 0:
+                    return "easy";
+                case 1:
+                    return "middle";
+                case 2:
+                    return "hard";
+            }
+            return null;
+        } catch (IOException e) {
+            //TO DO: реализовать свой класс Exctption
+            throw new RuntimeException(e);
+        }
+
     }
 }
