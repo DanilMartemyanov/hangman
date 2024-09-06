92873fea50be294a6774acff8bdfaf40faba159f
DanilMartemyanov <martidanilka@gmail.com>
2024-09-06 16:41:14 +0300

    correct steps in class ImageGallows

--- src/main/java/gallows/ImageGallows.java
+++ src/main/java/gallows/ImageGallows.java
@@ -46,12 +46,12 @@ public class ImageGallows {
 
     public static final String STEP_5 = "" +
         " __________________\n" +
-        "  |/              |\n" +
-        "  |               |\n" +
-        "  |\n " +
-        " |\n " +
-        " |\n " +
-        " |\n " +
+        " |/               |\n" +
+        " |                |\n" +
+        " |\n" +
+        " |\n" +
+        " |\n" +
+        " |\n" +
         "/|\\\n" +
         "                   ";
 
