3fbc8996f3fdaf6d548df50b5c7c47bf8a6d3b24
DanilMartemyanov <martidanilka@gmail.com>
2024-09-24 19:21:03 +0300

    regex in pattern

--- src/main/java/gallows/CheckInputUserImpl.java
+++ src/main/java/gallows/CheckInputUserImpl.java
@@ -8,16 +8,11 @@ import java.util.regex.Pattern;
 
 @SuppressWarnings("checkstyle:ConstantName")
 public class CheckInputUserImpl implements CheckInputUser {
-    static final String REGEXCATEGORY = "^[macnMACN]$";
-    static final Pattern patternCategory = Pattern.compile(REGEXCATEGORY);
-    static final String REGEXLEVEL = "^[emhnEMHN]$";
-    static final Pattern patternLevel = Pattern.compile(REGEXLEVEL);
-    static final String STARTGAME = "^[ynYN]$";
-    static final Pattern patternStartGame = Pattern.compile(STARTGAME);
-    static final String ENTERLETTER = "^[а-яА-Я1]$";
-    static final Pattern patternLetter = Pattern.compile(ENTERLETTER);
-    static final String GETTIP = "^[1\\S\s]$";
-    static final Pattern patternGetTip = Pattern.compile(GETTIP);
+    static final Pattern patternCategory = Pattern.compile("^[macnMACN]$");
+    static final Pattern patternLevel = Pattern.compile("^[emhnEMHN]$");
+    static final Pattern patternStartGame = Pattern.compile("^[ynYN]$");
+    static final Pattern patternLetter = Pattern.compile("^[а-яА-Я1]$");
+    static final Pattern patternGetTip = Pattern.compile("^[1\\S\s]$");
     static final String YES = "y";
     static final String NO = "n";
     static final String TIP = "1";
