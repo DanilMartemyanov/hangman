3e6d1a6b2f5ee1992223b5cffa09e12883180e32
DanilMartemyanov <martidanilka@gmail.com>
2024-09-25 11:31:02 +0300

    made the path to the file through the stream

--- src/main/java/gallows/Main.java
+++ src/main/java/gallows/Main.java
@@ -2,11 +2,13 @@ package gallows;
 
 import lombok.experimental.UtilityClass;
 
+
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
         GameInterface gameInterface = new GameInterface();
         gameInterface.userInterface();
     }
+
 }
 
--- src/main/java/gallows/WordsGallows.java
+++ src/main/java/gallows/WordsGallows.java
@@ -2,16 +2,26 @@ package gallows;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 @SuppressWarnings("checkstyle:MultipleStringLiterals")
 public class WordsGallows {
-    private static final File FILE = new File("src/main/resources/words.json");
+    private static InputStream file = getFileFromResourceAsStream("json/words.json");
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final JsonNode NODE;
     private SecureRandom random = new SecureRandom();
+
+    static {
+        try {
+            NODE = MAPPER.readTree(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String getRandomWords(ArrayList<String> words) {
         int quantity = words.size();
@@ -20,104 +30,79 @@ public class WordsGallows {
     }
 
     public String getRandomCategory() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode node = objectMapper.readTree(FILE);
-            ArrayList<String> category = new ArrayList<>();
-            for (JsonNode jsonNode1 : node.get("category")) {
-                category.add(jsonNode1.get("title").asText());
-            }
-            int index = random.nextInt(0, category.size());
-
-            return category.get(index).substring(0, 1).toLowerCase();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        ArrayList<String> category = new ArrayList<>();
+        for (JsonNode jsonNode1 : NODE.get("category")) {
+            category.add(jsonNode1.get("title").asText());
         }
+        int index = random.nextInt(0, category.size());
+
+        return category.get(index).substring(0, 1).toLowerCase();
 
     }
 
     public String getRandomLevel() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode node = objectMapper.readTree(FILE);
-            int sizeLevelArray = node.get("category").findValues("level").size();
-            int index = random.nextInt(0, sizeLevelArray);
-            switch (index) {
-                case 0:
-                    return "e";
-                case 1:
-                    return "m";
-                case 2:
-                    return "h";
-                default:
-                    return null;
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        int sizeLevelArray = NODE.get("category").findValues("level").size();
+        int index = random.nextInt(0, sizeLevelArray);
+        switch (index) {
+            case 0:
+                return "e";
+            case 1:
+                return "m";
+            case 2:
+                return "h";
+            default:
+                return null;
         }
-
     }
 
     public ArrayList<String> getAllCategories() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode node = objectMapper.readTree(FILE);
-            ArrayList<String> categories = new ArrayList<>();
-            for (JsonNode jsonNode : node.get("category")) {
-                categories.add(jsonNode.get("title").asText());
-            }
-            return categories;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        ArrayList<String> categories = new ArrayList<>();
+        for (JsonNode jsonNode : NODE.get("category")) {
+            categories.add(jsonNode.get("title").asText());
         }
+        return categories;
     }
 
     public JsonNode getJsonWords(String category, String levelUser) {
-        ObjectMapper objectMapper = new ObjectMapper();
         int indexCategory;
         int count = 0;
         String level;
-        try {
-            JsonNode node = objectMapper.readTree(FILE);
-            switch (category) {
-                case "m":
-                    indexCategory = 0;
-                    break;
-                case "a":
-                    indexCategory = 1;
-                    break;
-                case "c":
-                    indexCategory = 2;
-                    break;
-                default:
-                    indexCategory = -1;
-            }
-            switch (levelUser) {
-                case "e":
-                    level = "easy";
-                    break;
-                case "m":
-                    level = "middle";
-                    break;
-                case "h":
-                    level = "hard";
-                    break;
-                default:
-                    level = "";
-            }
-            for (JsonNode categoryNode : node.path("category")) {
-                for (JsonNode levelNode : categoryNode.path("level")) {
-                    for (JsonNode wordsNode : levelNode.path(level)) {
-                        if (indexCategory == count) {
-                            return wordsNode;
-                        }
-                        count++;
+        switch (category) {
+            case "m":
+                indexCategory = 0;
+                break;
+            case "a":
+                indexCategory = 1;
+                break;
+            case "c":
+                indexCategory = 2;
+                break;
+            default:
+                indexCategory = -1;
+        }
+        switch (levelUser) {
+            case "e":
+                level = "easy";
+                break;
+            case "m":
+                level = "middle";
+                break;
+            case "h":
+                level = "hard";
+                break;
+            default:
+                level = "";
+        }
+        for (JsonNode categoryNode : NODE.path("category")) {
+            for (JsonNode levelNode : categoryNode.path("level")) {
+                for (JsonNode wordsNode : levelNode.path(level)) {
+                    if (indexCategory == count) {
+                        return wordsNode;
                     }
+                    count++;
                 }
-
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
         }
         return null;
     }
@@ -135,5 +120,15 @@ public class WordsGallows {
 
     public boolean checkWord(String word) {
         return "".equals(word);
+    }
+
+    private static InputStream getFileFromResourceAsStream(String fileName) {
+        ClassLoader classLoader = WordsGallows.class.getClassLoader();
+        InputStream is = classLoader.getResourceAsStream(fileName);
+        if (is == null) {
+            throw new IllegalArgumentException("Resource not found");
+        } else {
+            return is;
+        }
     }
 }
